9266bfdb91145006bd0fe51ad6879f968bcbd931
RebeckaLWulf <rebeckawulf@gmail.com>
2025-04-03 21:01:48 +0200

    Update RandomForest.py

--- project 1/RandomForest.py
+++ project 1/RandomForest.py
@@ -2,14 +2,19 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
 
-def RF(test_set, train_set):
+
+def RF(trainingSet,trainingLabels,testSet):
     # Initialize the Classifier
+    # n_estimators: number of trees in the forest
+    # random_state: random speed for reproducibility
     clf = RandomForestClassifier(n_estimators=100, random_state=42)
 
     # Train the Classifier
-    clf.fit(train_set)
+    clf.fit(trainingSet, trainingLabels)
 
-    # Prediction
+    # Prediction using trained classifier on testSet
+    predictions = clf.predict(testSet)
 
-    # Evaluation of classifier
+    return predictions
