f5f1078cd1a308a18a45b4926faf9598a2183f2a
RebeckaLWulf <rebeckawulf@gmail.com>
2025-04-03 14:23:01 +0200

    Update RandomForest.py

--- project 1/RandomForest.py
+++ project 1/RandomForest.py
@@ -1,4 +1,12 @@
-#hello
+# Random forrest
 
-import numpy as np
-import matplotlib.pyplot as plt
+def RF(test_set, train_set):
+    # Initialize the Classifier
+    clf = RandomForestClassifier(n_estimators=100, random_state=42)
+
+    # Train the Classifier
+    clf.fit(train_set)
+
+    # Prediction
+
+    # Evaluation of classifier
