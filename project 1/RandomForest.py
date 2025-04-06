fe22787276474c26f8b3963def5e21532c2f12ce
RebeckaLWulf <rebeckawulf@gmail.com>
2025-04-06 23:42:37 +0200

    Update RandomForest.py

--- project 1/RandomForest.py
+++ project 1/RandomForest.py
@@ -22,6 +22,24 @@ def read_mnist_txt(file_path):
     
     return images, labels
 
+def randomForestClassifier(trainingSet, trainingLabels, testSet, n_estimators=100):
+    # Flatten 16x16 images to 256-length vectors for classification
+    X_train = [img.flatten() for img in trainingSet]
+    X_test = [img.flatten() for img in testSet]
+    
+    # Initialize Random Forest
+    # n_estimators: number of trees in the forest
+    # random_state: random speed for reproducibility
+    rf = RandomForestClassifier(n_estimators=100, random_state=42)
+    
+    # Train the Classifier
+    rf.fit(X_train, trainingLabels)
+    
+    # Predict using trained classifier on testSet
+    predictedLabels = rf.predict(X_test)
+
+    return predictedLabels
+
 # Load and prepare data
 print(os.getcwd())
 images, labels = read_mnist_txt(r"C:\Users\rebec\OneDrive\Dokument\GitHub\MVE441\project 1\Numbers.txt")
@@ -35,22 +53,8 @@ trainingLabels = labels[:len(images)-200]
 testSet = images[-200:]
 testSetLabels = labels[-200:]
 
-# Flatten 16x16 images to 256-length vectors for classification
-X_train = [img.flatten() for img in trainingSet]
-X_test = [img.flatten() for img in testSet]
-
-# def RF(trainingSet,trainingLabels,testSet):
-
-# Initialize Random Forest
-# n_estimators: number of trees in the forest
-# random_state: random speed for reproducibility
-rf = RandomForestClassifier(n_estimators=100, random_state=42)
-
-# Train the Classifier
-rf.fit(X_train, trainingLabels)
-
-# Predict using trained classifier on testSet
-predictedLabels = rf.predict(X_test)
+# Call the classifier
+predictedLabels = randomForestClassifier(trainingSet, trainingLabels, testSet, n_estimators=100)
 
 # Accuracy
 accuracy = accuracy_score(testSetLabels, predictedLabels) * 100
